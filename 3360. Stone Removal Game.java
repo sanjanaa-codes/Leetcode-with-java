fd720d1c500150693d238cac02e596e2a0cf1330
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-06-29 22:46:29 +0530

    Update 3360. Stone Removal Game.java

--- 3360. Stone Removal Game.java
+++ 3360. Stone Removal Game.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean canAliceWin(int n) {
-        int num = 10;
-        int a=0;
-        while(n>=num){
-            n-=num;
-            num--;
+        int remove= 10;
+        int a = 0;  //alice starts
+        while(n>=remove){
+            n-=remove;
+            remove--;
             a++;
         }
-        return a%2 !=0;
+        return a%2!=0;
     }
 }
+// n - 12, 2
+// remove - 10, 9
+// a - 1 (alice) , 3(alice)
+// a - 2 (bob) , 4(bob)
 
 ⏱️ Time Complexity : O(√n)
 🧠 Space Complexity : O(1) 
