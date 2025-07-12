e5cd5a9ca604a735db65ae77ce10816c9d05a381
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-07-12 23:34:13 +0530

    Create 1108. Defanging an IP Address.java

---
+++ Basics/1108. Defanging an IP Address.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.'){
+                sb.append("[.]");
+            }
+            else{
+                sb.append(address.charAt(i));
+            }
+           
+        }
+         return sb.toString();
+    }
+}
+
+_____________________________________________________________________________________
+
+class Solution {
+    public String defangIPaddr(String address) {
+         return address.replace(".","[.]");
+    }
+}
