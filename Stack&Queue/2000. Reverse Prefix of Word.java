1b84542f873d8a5a40338cd4a57d4cb7c2204b8d
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-09-06 15:47:31 +0530

    Create 2000. Reverse Prefix of Word.java

---
+++ Stack&Queue/2000. Reverse Prefix of Word.java
@@ -0,0 +1,20 @@
+Solution - 1 
+
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int i;
+        StringBuilder sb = new StringBuilder();
+        for( i=0;i<word.length();i++){
+            if(word.charAt(i)==ch){
+                sb.append(word.substring(0,i+1));
+                break;
+            }       
+        }
+        if(i==word.length()){
+            return word;
+        }
+        sb.reverse();
+        sb.append(word.substring(i+1,word.length()));
+        return sb.toString();
+    }
+}
