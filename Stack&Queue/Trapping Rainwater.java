8a2c08506c32dd4048e8ff68e0a839b5b58dce43
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-09-07 11:27:52 +0530

    Update Trapping Rainwater.java

--- Stack&Queue/Trapping Rainwater.java
+++ Stack&Queue/Trapping Rainwater.java
@@ -1,3 +1,35 @@
+class Solution {
+    public int trap(int[] height) {
+        int lmax = 0;
+        int rmax = 0;
+        int total = 0;
+        int l = 0;
+        int r = height.length - 1;
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] < lmax) {
+                    total += lmax - height[l];
+                } else {
+                    lmax = height[l];
+                }
+                l += 1;
+            } else {
+                if (height[r] < rmax) {
+                    total += rmax - height[r];
+                } else {
+                    rmax = height[r];
+                }
+                r -= 1;
+            }
+        }
+        return total;
+    }
+}
+
+TC -> O(N)
+SC -> O(!)
+
+
 For optimum approach
 ________________________________
 Intuition Behind the Problem:
